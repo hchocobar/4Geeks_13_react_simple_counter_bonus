bf2133004fb05fc62a7e80cb3e5af3638a2651ff
Héctor Chocobar-Torrejón <hector@chocobar.net>
2023-01-30 19:10:15 +0000

    feature: six digits

--- src/js/component/SimpleCounter.jsx
+++ src/js/component/SimpleCounter.jsx
@@ -4,6 +4,8 @@ import PropTypes from "prop-types";
 function SimpleCounter (props) {
     return ( <div className="bigCounter">
         <div><i className="far fa-clock"></i></div>
+        <div>{Math.floor(props.counter / 100000) % 10}</div>
+        <div>{Math.floor(props.counter / 10000) % 10}</div>
         <div>{Math.floor(props.counter / 1000) % 10}</div>
         <div>{Math.floor(props.counter / 100) % 10}</div>
         <div>{Math.floor(props.counter / 10) % 10}</div>
