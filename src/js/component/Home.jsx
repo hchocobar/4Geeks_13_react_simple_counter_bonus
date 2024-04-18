dc5a815401be655cb7417e0025389fb7df121520
Héctor Chocobar-Torrejón <hector@chocobar.net>
2024-04-18 03:00:00 +0000

    reveiw: spain-65

--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+// Import custom components
+import { SimpleCounter } from "./SimpleCounter.jsx";
+import { SimpleCounterBonus } from "./SimpleCounterBonus.jsx";
+// import { SimpleCounterBonus } from '.'
 
-//include images into your bundle
+// Include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
 		<div className="text-center">
+			<SimpleCounter />
+			<SimpleCounterBonus />
+			{/* 
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
 			<p>
 				<img src={rigoImage} />
@@ -19,6 +26,7 @@ const Home = () => {
 				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
 				love!
 			</p>
+ 			*/}
 		</div>
 	);
 };
